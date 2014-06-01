ba174f5faf9c901af3964820461e573f96fadf6b
Zapletal-Martin <zapletal-martin@email.cz>
2014-06-01 15:04:25 +0200

    Javascript refactoring continued. More functions extracted for modularity and readability.

--- public/javascripts/indexController.js
+++ public/javascripts/indexController.js
@@ -1,68 +1,67 @@
-(function() {
-    App.IndexController = Ember.ArrayController.extend({
-        appName: 'My First Example',
-        content: Ember.A(),
-        socket: new WebSocket("ws://192.168.0.15:9000/dashboardWebSocket"),
+App.IndexController = Ember.ArrayController.extend({
+    appName: 'My First Example',
+    content: Ember.A(),
+    socket: new WebSocket("ws://192.168.0.15:9000/dashboardWebSocket"),
 
-        init: function() {
-            var self = this;
-            self.removeUnusedDevicesTimer();
+    init: function() {
+        var self = this;
+        self.removeUnusedDevicesTimer();
 
 
-            self.socket.onmessage = function(event) {
-                self.handleWebSocketReceivedMessage(event);
-            };
-        },
+        self.socket.onmessage = function(event) {
+            self.handleWebSocketReceivedMessage(event);
+        };
+    },
 
-        removeUnusedDevicesTimer: function () {
-            function deviceDataUpdatedInSeconds(device, seconds) {
-                return (new Date().getTime() - seconds * 1000) < device.get("updated");
-            }
+    removeUnusedDevicesTimer: function () {
+        function deviceDataUpdatedInSeconds(device, seconds) {
+            return (new Date().getTime() - seconds * 1000) < device.get("updated");
+        }
 
-            var self = this;
-            Ember.run.later(this, function() {
-                self.get("content").forEach(function(device) {
-                    if(!deviceDataUpdatedInSeconds(device, 2)) {
-                        self.get("content").removeObject(device);
-                    }
-                });
-
-                this.removeUnusedDevicesTimer();
-            }, 1000);
-        },
-
-        handleWebSocketReceivedMessage: function(event) {
-            var self = this;
-            var json = JSON.parse(event.data);
-
-            json.data.alpha = THREE.Math.degToRad( json.data.alpha );
-            json.data.beta = THREE.Math.degToRad( json.data.beta );
-            json.data.gamma = THREE.Math.degToRad( json.data.gamma );
-
-            var device = undefined;
-
-            self.get("content").forEach(function(item) {
-                if(item.get("device") === json.device) {
-                    device = Device.update(item, json);
+        var self = this;
+        Ember.run.later(this, function() {
+            self.get("content").forEach(function(device) {
+                if(!deviceDataUpdatedInSeconds(device, 2)) {
+                    self.get("content").removeObject(device);
                 }
             });
 
-            if(device == undefined) {
-                var newDevice = Device.create(json);
-                self.get("content").pushObject(newDevice);
+            this.removeUnusedDevicesTimer();
+        }, 1000);
+    },
+
+    handleWebSocketReceivedMessage: function(event) {
+        var self = this;
+        var json = Orientation.convertDegreesToRadians(JSON.parse(event.data));
+
+        var device = undefined;
+
+        self.get("content").forEach(function(item) {
+            if(item.get("device") === json.device) {
+                device = Device.update(item, json);
             }
+        });
 
-            if(device != undefined && device.renderer == undefined) {
-                var canvas = document.getElementById('canvas' + device.deviceId);
+        if(!alreadyExists(device)) {
+            self.get("content").pushObject(Device.create(json));
+        }
 
-                if(canvas != undefined) {
-                    var renderer = Orientation.createCanvas(device.cube);
+        if(alreadyExists(device) && !alreadyExists(device.renderer)) {
+            var canvas = document.getElementById('canvas' + device.deviceId);
 
-                    device.set('cube', device.cube);
-                    device.set('renderer', renderer);
-                    canvas.appendChild(renderer.domElement);
-                }
+            if(canvasAlreadyRendered(canvas)) {
+                var renderer = Orientation.createCanvas(device.cube);
+                device.set('renderer', renderer);
+                canvas.appendChild(renderer.domElement);
             }
         }
-    });
-})();
+
+        function alreadyExists(obj) {
+            return obj !== undefined;
+        }
+
+        function canvasAlreadyRendered(canvas) {
+            return alreadyExists(canvas);
+        }
+    }
+});
